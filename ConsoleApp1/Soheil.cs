7ab9890cdc10f11f6669593deeedc057f69e0f0c
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-07-21 11:29:13 +0330

    Out Keyword 01

--- ConsoleApp1/Soheil.cs
+++ ConsoleApp1/Soheil.cs
@@ -1,3 +1,25 @@
 ﻿// this is first test for pull and push
 
 // test
+
+var variousNumbers = new int[] { 10, -8, 2, 12, -17 };
+int countOfNonPositiveNumbers;
+var onlyPositive = GetOnlyPositive(variousNumbers, out countOfNonPositiveNumbers);
+
+List<int> GetOnlyPositive(int[] numbers, out int countOfNonPositive)
+{
+    var result = new List<int>();
+    countOfNonPositive = 0;
+    foreach (var  number in numbers)
+    {
+        if (number > 0)
+        {
+            result.Add(number);
+        }
+        else
+        {
+            ++countOfNonPositive;
+        }
+    }
+    return result;
+}
