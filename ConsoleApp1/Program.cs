f661c296cac77098e854400225490941a0a98741
Kambiz <kambiz.fakhr@yahoo.com>
2025-07-21 11:47:12 +0330

    out keyword last commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -35,11 +35,40 @@ switch (userInput)
         break;
 
 }
-Console.ReadKey();
+
 
 void PrintSelectedOption(string selectedOption)
 {
     Console.WriteLine("Selected option: " + selectedOption);
 }
-  
 
+
+var variousNumbers = new int[] { 10, -8, 2, 12, -17 , -22};
+int countOfNonPositiveNumbers;
+var onlyPositive = GetOnlyPositive(variousNumbers, out countOfNonPositiveNumbers);
+
+List<int> GetOnlyPositive(int[] numbers, out int countOfNonPositive)
+{
+    var result = new List<int>();
+    countOfNonPositive = 0;
+    foreach (var number in numbers)
+    {
+        if (number > 0)
+        {
+            result.Add(number);
+        }
+        else
+        {
+            ++countOfNonPositive;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine(countOfNonPositiveNumbers);
+foreach (int number in onlyPositive)
+{
+    Console.WriteLine(number);
+}
+
+Console.ReadKey();
--- ConsoleApp1/Soheil.cs
+++ ConsoleApp1/Soheil.cs
@@ -2,30 +2,3 @@
 
 // test
 
-var variousNumbers = new int[] { 10, -8, 2, 12, -17 };
-int countOfNonPositiveNumbers;
-var onlyPositive = GetOnlyPositive(variousNumbers, out countOfNonPositiveNumbers);
-
-List<int> GetOnlyPositive(int[] numbers, out int countOfNonPositive)
-{
-    var result = new List<int>();
-    countOfNonPositive = 0;
-    foreach (var  number in numbers)
-    {
-        if (number > 0)
-        {
-            result.Add(number);
-        }
-        else
-        {
-            ++countOfNonPositive;
-        }
-    }
-    return result;
-}
-
-Console.WriteLine(countOfNonPositiveNumbers);
-foreach (int number in onlyPositive)
-{
-    Console.WriteLine(onlyPositive[number]);
-}
