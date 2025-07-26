d90e1c0ddeac9ebbdcc40f25878824d4a4581026
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-07-26 11:28:27 +0330

    OOP 01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,28 +1,35 @@
 ﻿
-byte myByteNum = 10;
-int myIntNum = 20;
-char myCharNum = 'Z';
-double myDoubleNum = 16;
+Product product1, product2, product3;
 
-Console.WriteLine(myByteNum);
-Console.WriteLine(default(byte));
-Console.WriteLine(byte.MaxValue);
-Console.WriteLine(byte.MinValue);
+product1 = new Product();
+product2 = new Product();
+product3 = new Product();
 
-Console.WriteLine(myIntNum);
-Console.WriteLine(default(int));
-Console.WriteLine(int.MaxValue);
-Console.WriteLine(int.MinValue);
+Product product4 = new Product();
 
-Console.WriteLine(myCharNum);
-Console.WriteLine(default(char));
-Console.WriteLine(char.MaxValue);
-Console.WriteLine(char.MinValue);
+product1.productID = 1001;
+product1.productName = "Mobile";
+product1.cost = 20000;
+product1.quantityInStock = 1200;
+product2.productID = 1002;
+product2.productName = "Laptop";
+product2.cost = 40000;
+product2.quantityInStock = 900;
+product3.productID = 1003;
+product3.productName = "Speaker";
+product3.cost = 10000;
+product3.quantityInStock = 1100;
 
-Console.WriteLine(myDoubleNum);
-Console.WriteLine(default(double));
-Console.WriteLine(double.MaxValue);
-Console.WriteLine(double.MinValue);
-
-Console.ReadKey();
+Console.WriteLine("Product 1:");
+Console.WriteLine("Product ID: " + product1.productID);
+Console.WriteLine("Product Name: " + product1.productName);
+Console.WriteLine("Product cost: " + product1.cost);
+Console.WriteLine("Quantity in stock: " + product1.quantityInStock);
+public class Product
+{
+    public int productID;
+    public string productName;
+    public double cost;
+    public int quantityInStock;
+}
 
