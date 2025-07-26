e40806fd107cdde1f59842fcc52edde87557108b
Kambiz <kambiz.fakhr@yahoo.com>
2025-07-26 11:51:06 +0330

    DateTime part 01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Globalization;
 Product product1, product2, product3;
 
 product1 = new Product();
@@ -7,10 +7,17 @@ product3 = new Product();
 
 Product product4 = new Product();
 
+PersianCalendar myPersianCalendar = new PersianCalendar();
+
+string myPersianConvertedDateTime = myPersianCalendar.GetYear(DateTime.Now) + "/" + myPersianCalendar.GetMonth(DateTime.Now) + "/" + myPersianCalendar.GetDayOfMonth(DateTime.Now);
+
 product1.productID = 1001;
 product1.productName = "Mobile";
 product1.cost = 20000;
 product1.quantityInStock = 1200;
+product1.birthDateTime = new DateTime(1990,01,20,0,0,0,0);
+//product1.persianbirthDateTime = Convert.ToString(new DateTime(2025, 07, 26, myPersianCalendar));
+product1.persianbirthDateTime = myPersianConvertedDateTime;
 product2.productID = 1002;
 product2.productName = "Laptop";
 product2.cost = 40000;
@@ -25,11 +32,17 @@ Console.WriteLine("Product ID: " + product1.productID);
 Console.WriteLine("Product Name: " + product1.productName);
 Console.WriteLine("Product cost: " + product1.cost);
 Console.WriteLine("Quantity in stock: " + product1.quantityInStock);
+Console.WriteLine("Product produce Date and Time: " + product1.birthDateTime);
+Console.WriteLine("Product produce Date and Time: " + product1.persianbirthDateTime);
+
+Console.ReadKey();
 public class Product
 {
     public int productID;
-    public string productName;
+    public string? productName;
     public double cost;
     public int quantityInStock;
+    public DateTime birthDateTime;
+    public string? persianbirthDateTime;
 }
 
