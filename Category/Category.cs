f865ed3325794e1a4cff538d11087046011d8605
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-08-23 13:08:29 +0330

    Struct memory allocation

---
+++ Category/Category.cs
@@ -0,0 +1,56 @@
+﻿namespace MyCategory
+{
+    public struct Category
+    {
+
+        private int _categoryID;
+        private string _categoryName;
+
+        public int CategoryID
+        {
+            set
+            {
+                if (value >= 1 && value <= 100)
+                {
+                    _categoryID = value;
+                }
+            }
+            get { return _categoryID; }
+            
+        }
+
+        public string CategoryName
+        {
+            set
+            {
+                if (value.Length <= 40)
+                {
+                    _categoryName = value;
+                }
+            }
+            get { return _categoryName; }
+        }
+
+        public Category(int categoryID, string categoryName)
+        {
+            _categoryID = categoryID;
+            _categoryName = categoryName;
+        }
+
+        public int GetCategoryNameLength()
+            { return _categoryName.Length; }
+
+
+    }
+
+    public struct Structure1
+    {
+        public int x, y;
+    }
+
+    public class Class1
+    {
+        public int x, y;
+    }
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,49 @@
-﻿
-byte myByteNum = 10;
-int myIntNum = 20;
-char myCharNum = 'Z';
-double myDoubleNum = 16;
+﻿using MyCategory;
 
-Console.WriteLine(myByteNum);
-Console.WriteLine(default(byte));
-Console.WriteLine(byte.MaxValue);
-Console.WriteLine(byte.MinValue);
+Category category = new Category(20, "General");
 
-Console.WriteLine(myIntNum);
-Console.WriteLine(default(int));
-Console.WriteLine(int.MaxValue);
-Console.WriteLine(int.MinValue);
+//category.CategoryID = 20;
+//category.CategoryName = "General";
 
-Console.WriteLine(myCharNum);
-Console.WriteLine(default(char));
-Console.WriteLine(char.MaxValue);
-Console.WriteLine(char.MinValue);
+Console.WriteLine(category.CategoryID);
+Console.WriteLine(category.CategoryName);
+Console.WriteLine(category.GetCategoryNameLength());
 
-Console.WriteLine(myDoubleNum);
-Console.WriteLine(default(double));
-Console.WriteLine(double.MaxValue);
-Console.WriteLine(double.MinValue);
+Structure1 structure1;
+structure1.x = 10;
+structure1.y = 20;
+
+Class1 class1;
+class1 = new Class1();
+class1.x = 10;
+class1.y = 20;
+
+Structure1 structure2;
+
+Class1 class2;
+
+structure2 = structure1;
+
+class2 = class1;
+
+structure2.x = 100;
+structure2.y = 200;
+
+class2.x = 100;
+class2.y = 200;
+
+Console.WriteLine(structure1.x);
+Console.WriteLine(structure1.y);
+Console.WriteLine(structure2.x);
+Console.WriteLine(structure2.y);
+
+Console.WriteLine(class1.x);
+Console.WriteLine(class1.y);
+Console.WriteLine(class2.x);
+Console.WriteLine(class2.y);
+
 
 Console.ReadKey();
+
+
+
